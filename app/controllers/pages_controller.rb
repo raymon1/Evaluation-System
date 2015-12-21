b6f962e5ceaf5d6523c835875f8c1d907b8be46b
Mohamed Ahmed <mohamed71992@gmail.com>
2015-12-21 21:25:28 +0200

    login working good

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,22 +7,24 @@ class PagesController < ApplicationController
 
 	def home
 		user = find_user params[:email_entered],params[:password_entered]
+    @username = params[:email_entered]
+    @pass_zeft = params[:password_entered]
 	    if (@role == "student")
 	      sign_in user    
-	      redirect_to '/users/index'
+	    #  redirect_to '/users/index'
 	    elsif (@role == "instructor")
 	      sign_in user
-	      redirect_to '/users/index'  
+	   #   redirect_to '/users/index'  
 	    end
 	end
 
 
  private
     def find_user (email,password)
-      if user = User.find_by_email(email) and User.valid_password?(password)
-      	 if (Student.find_by_studentID(user.collegeID))
+      if user = User.find_by_email(email) and user.valid_password?(password)
+      	 if (Student.find_by_studentid(user.collegeid))
       	 	 @role = "student"
-      	 elsif (Instructor.find_by_instructorID(user.collegeID))
+      	 elsif (Instructor.find_by_instructorid(user.collegeid))
       	 	 @role = "instructor"
       	 else
       	 	 @role = "admin"
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 
-  get 'pages/home' => 'pages#home'
+  post 'pages/home' => 'pages#home'
 
   devise_for :users
 
