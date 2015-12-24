514133fa6439d0de369cde9f65157a62b22467cf
Mohamed Ahmed <mohamed71992@gmail.com>
2015-12-24 18:21:51 +0200

    before arwa

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
-  # skip_before_filter  :verify_authenticity_token , only: [:]
 
 private
 	def after_sign_in_path_for(user)
-		puts "=========================="
 	   if (Student.find_by_student_id(user.college_id))
-	   		puts "+++++++++++++++++++++++++++"
 	   		return '/student'
 	   else
-	   		puts "-----------------------------------"
 	   		return '/instructor/home' 	      
 	    end
 	end
--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -1,10 +1,12 @@
 class InstructorsController < ApplicationController
 
+
+	before_filter :authenticate_user!
+
 	def home
 		
-		
-
-
+		instructor = Instructor.find(current_user.college_id)
+		@courses = instructor.courses
 	end
 
 end
