baaba66b7c0d8fd01bfd40e1ba77a95b3bb9c93b
Mohamed Ahmed <mohamed71992@gmail.com>
2015-12-25 20:15:16 +0200

    submit a quiz issue

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -5,7 +5,7 @@ class QuizzesController < ApplicationController
 	end
 
 	def create
-		
+		redirect_to "/instructor/home"
 	end
 
 	def add
@@ -15,12 +15,27 @@ class QuizzesController < ApplicationController
 	end
 
 	def add_mcq
+		if (@mcq_count.nil?)
+			@mcq_count = 0
+			@mcq = []
+		else
+			@mcq_count = @mcq_count.next
+		end
+		@mcq[@mcq_count]={}
+
 		  respond_to do |format|               
     		format.js {render "/forms/make_question/add_mcq"}
   		end 
 	end
 
 	def add_tf
+		 if (@tf_count.nil?)
+		 	@tf_count = 0
+		 	@tf = []
+		 else
+		@tf_count = @tf_count.next
+	end
+		@tf[@tf_count]={}
 		  respond_to do |format|               
     		format.js {render "/forms/make_question/add_tf"}
   		end 
@@ -29,6 +44,13 @@ class QuizzesController < ApplicationController
 	end
 
 	def add_essay
+		 if (@essay_count.nil?)
+		 	@essay_count = 0
+		 	@essay = []
+		 else
+		@essay_count = @essay_count.next
+	end
+		@essay[@essay_count]={}
 		respond_to do |format|               
     		format.js {render "/forms/make_question/add_essay"}
   		end 
