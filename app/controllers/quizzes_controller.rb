a824f50e471ebb6c3705ba56559ce189b409bc34
raymon1 <raymonmina@gmail.com>
2015-12-26 04:05:22 +0200

    creating quiz controller, I broke The rails and chose the dark side

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -5,6 +5,43 @@ class QuizzesController < ApplicationController
 	end
 
 	def create
+		puts params
+		form = Form.create(course_id: params[:code],
+			instructor_id: current_user.college_id,
+			title: params["title"], creation_time_date: Time.now)
+
+		dur = params["duration"].to_f
+		quiz_mark = params["mark"].to_f
+		pub_date = params["publish_date"].to_time
+
+		quiz = Quiz.create(quiz_id: form.form_id, duration: dur, quiz_mark: quiz_mark, publish_date: pub_date)
+
+		
+		mcq_count = params[mcq_counter]
+		for i in 1..mcq_count
+			qmark = params["mcq_question_mark" + i.to_s]
+			question = params["mcq_question" + i.to_s]
+			choice1 = params["mcq_answer_ch1" + i.to_s]
+			choice2 = params["mcq_answer_ch2" + i.to_s]
+			hmark = params["mcq_hint_mark" + i.to_s]
+			hint = params["mcq_hint" + i.to_s]
+			bonus = params["bonus" + i.to_s]==""? false : true
+			if params["mcq_answer_ch1" + i.to_s] == "option1"
+				answer=choice1
+			else
+				answer=choice2
+			end
+
+			ques = Question.create(form_id: form.form_id, question: question, mark: qmark, hint: hint, hint_mark: hmark, bonus: bonus)
+			Mcq.create(mcq_id: ques.question_id, answer: answer)
+			McqChoice.create(question_id: ques.question_id, choice: choice1)
+			McqChoice.create(question_id: ques.question_id, choice: choice2)
+		end
+
+
+
+
+
 		redirect_to "/instructor/home"
 	end
 
